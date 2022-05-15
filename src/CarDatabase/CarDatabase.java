e1d81c73512a0fef22b1adb4579a18b706538368
Szymon Sulejczak <contact@szymonsulejczak.com>
2022-05-15 23:46:21 +0200

    add new functions

--- src/CarDatabase/CarDatabase.java
+++ src/CarDatabase/CarDatabase.java
@@ -1,4 +1,26 @@
 package CarDatabase;
+import Car.Car;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarDatabase {
+    private List<Car> cars = new ArrayList<Car>();
+
+    public CarDatabase() {};
+
+    public List<Car> getCars() {
+        return this.cars;
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public void deleteCar(int index) {
+        cars.remove(index - 1);
+    }
+
+    public void updateCar(int index, Car car) {
+        cars.set(index - 1, car);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,106 @@
 import Car.Car;
+import CarDatabase.CarDatabase;
+
+import java.io.Console;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        System.out.println(car);
+
+        String mark, model, color;
+        int productionDate, course, doorQuantity, index;
+
+        CarDatabase base = new CarDatabase();
+        Scanner scan = new Scanner(System.in);
+
+        char zn;
+
+        do {
+            System.out.println("1. Wyświetl auta");
+            System.out.println("2. Dodaj auto");
+            System.out.println("3. Usuń auto");
+            System.out.println("4. Aktualizuj auto");
+            System.out.println("x. Wyjdź z programu");
+            System.out.flush();
+
+            zn = scan.next().charAt(0);
+
+            switch (zn) {
+                case '1':
+                {
+                    for(Car c : base.getCars()) {
+                        System.out.println("marka: " + c.getMark());
+                        System.out.println("model: " + c.getModel());
+                        System.out.println("data produkcji: " + c.getProductionDate());
+                        System.out.println("kolor: " + c.getColor());
+                        System.out.println("przebieg: " + c.getCourse());
+                        System.out.println("ilość drzwi: " + c.getDoorQuantity());
+                    }
+                    break;
+                }
+                case '2':
+                {
+                    scan.nextLine();
+                    System.out.println("Podaj markę samochodu: ");
+                    mark = scan.nextLine();
+                    System.out.println("Podaj model samochodu: ");
+                    model = scan.nextLine();
+                    System.out.println("Podaj datę produkcji samochodu: ");
+                    productionDate = scan.nextInt();
+                    scan.nextLine();
+                    System.out.println("Podaj kolor samochodu: ");
+                    color = scan.nextLine();
+                    System.out.println("Podaj przebieg samochodu: ");
+                    course = scan.nextInt();
+                    scan.nextLine();
+                    System.out.println("Podaj ilość drzwi samochodu: ");
+                    doorQuantity = scan.nextInt();
+                    scan.nextLine();
+                    Car car = new Car(mark, model, productionDate, color, course, doorQuantity);
+                    base.addCar(car);
+                    System.out.println("DODANO NOWE AUTO!");
+                    break;
+                }
+                case '3':
+                {
+                    System.out.println("Podaj index auta które chcesz usunąć: ");
+                    index = scan.nextInt();
+                    scan.nextLine();
+                    base.deleteCar(index);
+                    System.out.println("Usunięto auto z indexem " + index);
+                    break;
+                }
+                case '4':
+                {
+                    scan.nextLine();
+                    System.out.println("Podaj markę samochodu: ");
+                    mark = scan.nextLine();
+                    System.out.println("Podaj model samochodu: ");
+                    model = scan.nextLine();
+                    System.out.println("Podaj datę produkcji samochodu: ");
+                    productionDate = scan.nextInt();
+                    scan.nextLine();
+                    System.out.println("Podaj kolor samochodu: ");
+                    color = scan.nextLine();
+                    System.out.println("Podaj przebieg samochodu: ");
+                    course = scan.nextInt();
+                    scan.nextLine();
+                    System.out.println("Podaj ilość drzwi samochodu: ");
+                    doorQuantity = scan.nextInt();
+                    scan.nextLine();
+                    System.out.println("Podaj index auta które chcesz zaktualizować: ");
+                    index = scan.nextInt();
+                    scan.nextLine();
+                    Car car = new Car(mark, model, productionDate, color, course, doorQuantity);
+                    base.updateCar(index, car);
+                    System.out.println("Zaktualizowano auto z indexem " + index);
+                    break;
+                }
+                case 'x':
+                    System.out.println("DO WIDZENIA!");
+                    break;
+            }
+        } while(zn!= 'x');
     }
 }
